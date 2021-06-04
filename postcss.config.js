fc1461cfeb6735cb0ec1bff6fe5114c60f7bb4e4
Vince Picone <vp@vincepic.one>
2021-06-04 17:40:34 -0500

    chore: fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,18 @@
 const customPlugins =
   process.env.NODE_ENV === 'production'
     ? [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
-          ],
-          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-          safelist: ['html', 'body'],
-        },
+        [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+            safelist: ['html', 'body'],
+          },
+        ],
       ]
     : [];
 
